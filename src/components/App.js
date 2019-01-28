767a37989fa8f190da8b5fea572556e35f242170
gimjudge <gimjudge@hotmail.com>
2019-01-28 13:35:46 -0800

    react Router Basics

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+
+// App Components
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Courses from './Courses';
+import Teachers from './Teachers';
+import NotFound from './NotFound';
+
+const App = () => (
+  <BrowserRouter>
+	<div className="container">
+		<Header></Header>
+		<Route exact path="/" component={Home} />
+		<Route path="/about" render={ () => <About title='About' /> } />
+		<Route path="/courses" component={Courses} />
+		<Route path="/teachers" component={Teachers} />
+	</div>
+  </BrowserRouter>
+);
+
+export default App;
